74c1a399ff5163f3d45ac95e2764e5ed9c5d22e8
Anne Küsters <a.kuesters@fz-juelich.de>
2018-09-20 09:53:04 +0200

    build up osx and linux in matrix

--- hello.cpp
+++ hello.cpp
@@ -6,5 +6,6 @@ int main()
 {
   std::cout << "Hello World!" << std::endl;
   std::cout << "Testing Travis CI on Mac!" << std::endl;
+  std::cout << "Testing Travis CI on Linux!" << std::endl;
   return 0;
 }
