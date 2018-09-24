82ec9ef70b05e55933a378ad43c5b82c2da97788
Anne Küsters <a.kuesters@fz-juelich.de>
2018-09-24 11:31:20 +0200

    added libstdc++-6-dev package

--- hello_mpi.cpp
+++ hello_mpi.cpp
@@ -2,9 +2,23 @@
  
 #include <iostream>
 #include <mpi.h>
+
+#include <memory>
  
-int main(int argc, char** argv)
+struct Vec3
 {
+    int x, y, z;
+    Vec3() : x(0), y(0), z(0) { }
+    Vec3(int x, int y, int z) :x(x), y(y), z(z) { }
+    friend std::ostream& operator<<(std::ostream& os, Vec3& v) {
+        return os << '{' << "x:" << v.x << " y:" << v.y << " z:" << v.z  << '}';
+    }
+};
+ 
+int main()
+{
+    auto v = std::make_unique<Vec3>();
+ 
     MPI_Init(&argc, &argv);
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
